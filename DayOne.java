bca9d552ccf485022e0a891a030c90658f26959f
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-12-20 19:41:03 -0800

    Complete class javadoc

--- DayOne.java
+++ DayOne.java
@@ -1,5 +1,11 @@
 import java.io.*;
 import java.util.*;
+/**
+ * Day One Challenge
+ * 
+ * @author Siddharth Sancheti
+ * @version December 20, 2023
+ */
 public class DayOne {
     public static void main(String[] args) {
         ArrayList<String> input = new ArrayList<>();
