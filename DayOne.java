094cae3c92f47b62ca807a4c1425ed8a76ba8cdd
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-12-20 17:56:52 -0800

    First attempt

--- DayOne.java
+++ DayOne.java
@@ -1,5 +1,15 @@
 public class DayOne {
     public static void main(String[] args) {
-        
+        int sum = 0;
+        for (String arg : args) {
+            String[] nums = arg.split("[a-zA-Z]");
+            if (nums.length == 1) {
+                sum += Integer.parseInt(nums[0]) * 10 + Integer.parseInt(nums[0]);
+            } else {
+                sum += Integer.parseInt(nums[0]) * 10 + Integer.parseInt(nums[nums.length - 1]);
+            }
+
+        }
+        System.out.println(sum);
     }
 }
