e4cfd1ec89674857e3f2f9d94e5804eff0366ec5
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-12-20 18:22:29 -0800

    Part One complete

--- DayOne.java
+++ DayOne.java
@@ -1,15 +1,39 @@
+import java.io.*;
+import java.util.*;
 public class DayOne {
     public static void main(String[] args) {
-        int sum = 0;
-        for (String arg : args) {
-            String[] nums = arg.split("[a-zA-Z]");
-            if (nums.length == 1) {
-                sum += Integer.parseInt(nums[0]) * 10 + Integer.parseInt(nums[0]);
-            } else {
-                sum += Integer.parseInt(nums[0]) * 10 + Integer.parseInt(nums[nums.length - 1]);
+        ArrayList<String> input = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                input.add(line);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(partOne(input));
+    }
+
+    public static int partOne(ArrayList<String> input) {
+        int sum = 0;
+        for (String arg : input) {
+            boolean firstDigitTaken = false;
+            int firstDigit = 0, secondDigit = 0;
+            for (int i = 0; i < arg.length(); i++) {
+                if (Character.isDigit(arg.charAt(i))) {
+                    if (!firstDigitTaken) {
+                        firstDigit = Character.getNumericValue(arg.charAt(i));
+                        secondDigit = firstDigit;
+                        firstDigitTaken = true;
+                    } else {
+                        secondDigit = Character.getNumericValue(arg.charAt(i));
+                    }
+                }
+            }
+            sum += firstDigit * 10 + secondDigit;
 
         }
-        System.out.println(sum);
+
+        return sum;
     }
 }
